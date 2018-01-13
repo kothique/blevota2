cd81736bd13890fd44d936407df703fbef16f629
Kothique Félixx <kothique@protonmail.ch>
2018-01-13 18:03:18 +0300

    Add an empty react app

---
+++ client/ui/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class App extends React.Component {
+  render = () => <h3>Hi there!</h3>
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = [
+  {
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, 'client'),
+      filename: 'bundle.js'
+    },
+    context: path.resolve(__dirname, 'client'),
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: path.resolve(__dirname, 'node_modules'),
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [
+      
+    ]
+  }
+]
