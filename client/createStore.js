6044d8680789ac22842435ac08d874cc1857098d
Kothique Félixx <kothique@protonmail.ch>
2018-01-20 13:34:16 +0300

    Use redux-logger

--- client/createStore.js
+++ client/createStore.js
@@ -2,16 +2,25 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import compose from 'redux/src/compose'
 import thunk from 'redux-thunk'
 import { routerReducer, routerMiddleware, routerActions } from 'react-router-redux'
+import logger from 'redux-logger'
 
 import reducers from './reducers'
 
-export default (history) => createStore(
-  combineReducers({
-    ...reducers,
-    router: routerReducer
-  }),
-  applyMiddleware(
+export default (history) => {
+  let middlewares = [
     thunk,
     routerMiddleware(history)
+  ]
+
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(logger)
+  }
+
+  return createStore(
+    combineReducers({
+      ...reducers,
+      router: routerReducer
+    }),
+    applyMiddleware(...middlewares)
   )
-)
+}
