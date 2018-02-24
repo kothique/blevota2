8ea84a1e49d33156283e0c0f9f776c49452a3f84
Kothique Félixx <kothique@protonmail.ch>
2018-02-24 19:53:33 +0300

    Test compatibility between client and server effects

---
+++ tests/serialization/effects/speedup.js
@@ -0,0 +1,23 @@
+const ServerSpeedUp = require('../../../server/game/effects/speedup')
+const { default: ClientEffect } = require('../../../client/game/effect')
+require('../../../client/game/effects/speedup')
+const { SPEEDUP } = require('../../../common/effects')
+
+describe('SpeedUp effect serialization', () => {
+  test('deserialized effect should match the serialized one', () => {
+    const serverEffect = ServerSpeedUp.create(42),
+          serverLength = serverEffect.serializedLength(),
+          buffer = Buffer.alloc(serverLength)
+
+    serverEffect.serialize(buffer)
+
+    const {
+      effect: clientEffect,
+      offset: clientLength
+    } = ClientEffect.deserialize(buffer)
+
+    expect(serverLength).toBe(clientLength)
+    expect(clientEffect.type).toBe(SPEEDUP)
+    expect(clientEffect.value).toBe(42)
+  } )
+})
